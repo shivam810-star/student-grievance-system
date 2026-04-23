b5913f3e3fd177bab9b3f62544793e55bae95760
shivam176 <shivamks1810@gmai.com>
2026-04-23 15:33:50 +0530

    frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://student-grievance-system-2.onrender.com/api",
 });
 
-// Attach token automatically
+// attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
